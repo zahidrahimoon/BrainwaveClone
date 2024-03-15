d7db9a8f89202b2797dce4e9db0dcd0f32bed083
*Muhammad <*idreesnawab78@gmail.com*>
2024-03-14 22:48:39 -0700

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import ButtonGradient from "./assets/assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Button from "./components/Button";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          {" "}
+          SomeThing
+        </Button>
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
